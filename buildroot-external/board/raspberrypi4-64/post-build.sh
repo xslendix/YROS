3cc832abb81017ca27f2ee03ce1f98718364261f
xSlendiX <gamingxslendix@gmail.com>
2022-04-03 02:07:46 +0300

    Fix RPi4's config.txt be wrongly copied.
    Before, for RPi4s, the config.txt was the one from the RPi3 configuration.
    This commit changes that so the system becomes bootable and has the correct
    boot-specific configuration.

--- buildroot-external/board/raspberrypi4-64/post-build.sh
+++ buildroot-external/board/raspberrypi4-64/post-build.sh
@@ -1,1 +1,36 @@
-../raspberrypi/post-build.sh
+#!/bin/sh
+
+set -u
+set -e
+
+BOARD_DIR="$(dirname $0)"
+
+. "${BR2_EXTERNAL_YROS_PATH}/meta"
+. "${BOARD_DIR}/meta"
+
+# Write os-release
+{
+    echo "NAME=\"${YROS_NAME}\""
+    echo "VERSION=\"${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD} ${BOARD_NAME}\""
+    echo "ID=${YROS_ID}"
+    echo "PRETTY_NAME=\"${YROS_NAME} ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD} ${BOARD_NAME}\""
+    echo "HOME_URL=https://github.com/xslendix/YROS"
+    echo "SUPPORT_URL=https://github.com/xslendix/YROS/issues"
+} > "${TARGET_DIR}/usr/lib/os-release"
+
+# Write machine-info
+{
+    echo "CHASSIS=${CHASSIS}"
+    echo "DEPLOYMENT=${DEPLOYMENT}"
+} > "${TARGET_DIR}/etc/machine-info"
+
+cp -vf ../buildroot-external/board/raspberrypi4-64/config.txt ${BINARIES_DIR}/rpi-firmware/config.txt
+cp -vf ../buildroot-external/board/raspberrypi4-64/cmdline.txt ${BINARIES_DIR}/rpi-firmware/cmdline.txt
+
+rm -f "${TARGET_DIR}/usr/lib/systemd/system/pigpio.service"
+
+vim "${TARGET_DIR}/etc/yrosd.conf"
+
+# cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+# chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+# cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
